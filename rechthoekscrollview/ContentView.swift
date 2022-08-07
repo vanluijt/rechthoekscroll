8b6961020d132a74d8019fd490adcf12752355c8
vanluijt <vl54@ziggo.nl>
2022-08-07 21:05:04 +0200

    Initial Commit

---
+++ rechthoekscrollview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rechthoekscrollview
+//
+//  Created by P.A. van Luijt on 07/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rechthoekscrollview/rechthoekscrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  rechthoekscrollviewApp.swift
+//  rechthoekscrollview
+//
+//  Created by P.A. van Luijt on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct rechthoekscrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
